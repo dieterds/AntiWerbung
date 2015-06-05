6b5466b4c0d7121159b63cfd90662c6858bd68f8
dieterds <bytubed2@gmail.com>
2015-06-05 14:42:15 +0200

    N24 Mediathek

---
+++ AntiWerbung_fuer_N24_Mediathek.user.js
@@ -0,0 +1,15 @@
+﻿// ==UserScript==
+// @name        Antiwerbung fuer N24 Mediathek
+// @namespace   N24
+// @description Entfernt die Videowerbung, die vor jedem Video abgespielt wird.
+// @include     http://www.n24.de/n24/Mediathek/videos/*
+// @downloadURL https://raw.githubusercontent.com/dieterds/AntiWerbung/master/AntiWerbung_fuer_N24_Mediathek.user.js
+// @updateURL   https://raw.githubusercontent.com/dieterds/AntiWerbung/master/AntiWerbung_fuer_N24_Mediathek.user.js
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+console.log('AntiWerbung fuer N24 Mediathek startet');
+_n24VideoCfg.adEnabled = false;
+//_n24VideoCfg.flash.autoPlay = false;
+//n24VideoAutoStart = false;
+//delete myAd;
