dd10ed8b08086f7b6d2fa17b3c1c988b40c600a3
dieterds <bytubed2@gmail.com>
2015-06-16 16:16:10 +0200

    Revert "Revert "Fix""
    This reverts commit 9605d8fc444a4d4b2d0c245c7ac86a4df2041705.

--- AntiWerbung_fuer_Winfuture.de.user.js
+++ AntiWerbung_fuer_Winfuture.de.user.js
@@ -20,19 +20,16 @@ console.log('AntiWerbung fuer Winfuture.de startet');
   return injected;
 }*/
 var allElements,
-thisElement;
+  thisElement;
 var videoIDArray = [
 ];
 var videoID;
 allElements = document.getElementsByTagName('*');
 for (var i = 0; i < allElements.length; i++) {
   thisElement = allElements[i];
-  if (thisElement.id.match(/player(\d+)/))
-  {
-    asel = thisElement;
+  if (thisElement.id.match(/player(\d+)/)) {
     videoID = RegExp.$1;
-    if (videoIDArray.indexOf(videoID) == - 1)
-    {
+    if (videoIDArray.indexOf(videoID) == - 1) {
       console.log('Video id is: ' + videoID);
       videoIDArray[videoIDArray.length] = videoID;
       //break;
@@ -41,15 +38,12 @@ for (var i = 0; i < allElements.length; i++) {
   // irgendetwas mit diesem Element tun
 
 }
-if (allElements.length == 0)
-{
+if (allElements.length == 0) {
   console.log('No Elements found!');
 }
-for (index = 0; index < videoIDArray.length; index++)
-{
+for (var index = 0; index < videoIDArray.length; index++) {
   videoID = videoIDArray[index];
-  if (videoID)
-  {
+  if (videoID) {
     console.log('Bearbeite ' + videoID);
     var tester = $f('player' + videoID).getConfig();
     console.log(tester);
