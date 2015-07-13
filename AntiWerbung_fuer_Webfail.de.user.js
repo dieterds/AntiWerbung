98542cff173f775d49f4ff411b8197cc1c8786e0
dieterds <bytubed2@gmail.com>
2015-07-13 15:58:35 +0200

    Update to generic

--- AntiWerbung_fuer_Webfail.de.user.js
+++ AntiWerbung_fuer_Webfail.de.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AntiWerbung fuer Webfail
 // @namespace   WEBFAIL
-// @description Entfernt die Videowerbung, die vor jedem Video abgespielt wird.
+// @description Entfernt die Werbung, jedes mal dynamisch neu erzeugt wird.
 // @include     http://de.webfail.*/*
 // @downloadURL https://raw.githubusercontent.com/dieterds/AntiWerbung/master/AntiWerbung_fuer_Webfail.user.js
 // @updateURL   https://raw.githubusercontent.com/dieterds/AntiWerbung/master/AntiWerbung_fuer_Webfail.user.js
@@ -9,11 +9,26 @@
 // @version     1.0
 // @grant       none
 // ==/UserScript==
+console.log('AntiWerbung fuer Webfail startet');
+window.setTimeout(function ()
+{
+  for each(var UnwantedVar in 'uabInject uabBody uabDetect uabTrack uabpRnd uabPc uabpd1 uabpd2 uabpd3 uabpdl UABPdd'.split(' '))
+  {
+    window[UnwantedVar] = null;
+    delete window[UnwantedVar];
+    //console.log(UnwantedVar);
+  }
+}, 500);
+/*$(document).ready(function ()
+{
   console.log('AntiWerbung fuer Webfail startet');
   var timer = window.setInterval(function () {
     $('#subheader').next().hide();
+    $('#aside').children()[3].remove()
     console.log('hidehidehide');
   }, 200)
   window.setTimeout(function () {
     clearInterval(timer);
   }, 1000);
+});
+*/
