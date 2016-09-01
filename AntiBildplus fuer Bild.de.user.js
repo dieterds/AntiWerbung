e55458c42f18cb5f66e78badf05fc655136a5b30
dieterds <bytubed2@gmail.com>
2016-09-01 13:55:08 +0200

    Update

---
+++ AntiBildplus fuer Bild.de.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        AntiBildplus fuer Bild.de
+// @namespace   bild.de
+// @description Entfernt alle BildPlus-Artikel von den Seiten.
+// @include     http://www.bild.de/*
+// @require     http://code.jquery.com/jquery-1.11.2.min.js
+// run-at document-start
+// @icon        https://raw.githubusercontent.com/dieterds/AntiWerbung/master/antiicon-48.png
+// @version     1.1
+// @grant       none
+// ==/UserScript==
+/*window.setInterval(function () {
+  window.UABPMsg = function () {
+    console.log('haha');
+  }
+}, 500);*/
+$('.bplus, .variant').remove();
+$('#logo').remove();
+$('a[href*="bild-plus"]').remove();
+$('img[src*="gewinnspiel"]').remove();
